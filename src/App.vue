47cdeb9cdee5ad5d5f6893a54efc3ecbb6ca0fb6
Thea Ponleu <ponleu913@gmail.com>
2026-08-14 13:50:38 +0700

    Add vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 
   
 <template>
+    <SpeedInsights/>
     <Layout/>
 </template>
 
 <script setup lang="ts">
 import Layout from './layout/Layout.vue';
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <style scoped>
